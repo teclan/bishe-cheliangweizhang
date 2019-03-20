d6e5584b0c47e1cdab92e063e4ea68f1b77a1311
teclan <tbj621@163.com>
2019-03-20 15:49:03 +0800

    修改用户表结构

--- src/main/resources/db/migration/V1_0001__create_table_user_info.sql
+++ src/main/resources/db/migration/V1_0001__create_table_user_info.sql
@@ -5,7 +5,9 @@ CREATE TABLE `user_info` (
   `password` varchar(64) comment '登录密码',
   `age` int comment '年龄',
   `phone` varchar(16) comment '电话',
-  `role` varchar(32) comment '角色，superadmin/admin/general:超级管理员/管理员/普通用户'
+  `role` varchar(32) comment '角色，superadmin/admin/general:超级管理员/管理员/普通用户',
+  `token` varchar(32) comment 'token',
+  `last_time` datetime comment '最后操作时间'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
