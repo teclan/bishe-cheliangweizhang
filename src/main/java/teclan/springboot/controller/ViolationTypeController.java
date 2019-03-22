e2b1a7159bb68bfb8c729be50c8b553d0dde60ed
teclan <tbj621@163.com>
2019-03-22 16:51:07 +0800

    添加查询违章类型列表接口

---
+++ src/main/java/teclan/springboot/controller/ViolationTypeController.java
@@ -0,0 +1,28 @@
+package teclan.springboot.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import teclan.springboot.utils.ResultUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 违章类型查询
+ */
+@RestController
+@RequestMapping("/ViolationType")
+public class ViolationTypeController {
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+
+    @RequestMapping("/get")
+    public JSONObject get() {
+        List<Map<String,Object>> mapList = jdbcTemplate.queryForList("select * from  violation_type");
+        return ResultUtils.get("查询成功", mapList);
+    }
+}
