8589f4951c772daab13c4f43a505f5354608268c
teclan <tbj621@163.com>
2019-05-09 14:50:05 +0800

    '修复缺失token时的异常'

--- src/main/java/teclan/springboot/filter/SessionFilter.java
+++ src/main/java/teclan/springboot/filter/SessionFilter.java
@@ -1,5 +1,6 @@
 package teclan.springboot.filter;
 
+import com.mysql.cj.util.StringUtils;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,14 +32,24 @@ public class SessionFilter implements Filter {
         HttpServletRequest httpServletRequest =(HttpServletRequest)servletRequest;
         String user = httpServletRequest.getHeader("user");
         String token =httpServletRequest.getHeader("token");
-
-        String realToken = jdbcTemplate.queryForObject(String.format("select token from user_info where code='%s'",user),String.class);
+        String realToken="";
 
         HttpServletResponse httpServletResponse =(HttpServletResponse) servletResponse;
+
+        if(StringUtils.isNullOrEmpty(user)){
+            LOGGER.error("\n\n {} , token无效,缺失[user]字段 ...",httpServletRequest.getRequestURI());
+            httpServletResponse.setStatus(403);
+            return;
+        }else{
+            realToken = jdbcTemplate.queryForObject(String.format("select token from user_info where code='%s'",user),String.class);
+        }
+
+
         if ( !"/user/login".equals(httpServletRequest.getRequestURI()) && (token == null || "".equals(token.toString()) || !token.equals(realToken) )) {
             LOGGER.error("未认证");
             httpServletResponse.setStatus(401);
             jdbcTemplate.update("update user_info set token=?,last_time=? where code=?",null,Constants.SDF.format(new Date()),user);
+            return;
         }else {
             jdbcTemplate.update("update user_info set last_time=? where code=?",new Date(),user);
             filterChain.doFilter(servletRequest,servletResponse);
