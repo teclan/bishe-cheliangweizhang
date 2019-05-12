ec56cfc7836027231d19adb8ed27622fd2e04d76
teclan <tbj621@163.com>
2019-05-12 21:55:51 +0800

    '修改sql'

--- src/main/resources/db/migration/V1_0009__alter_user_info_column_type.sql
+++ src/main/resources/db/migration/V1_0009__alter_user_info_column_type.sql
@@ -1,2 +1,2 @@
-ALTER TABLE cheliangweizhang.user_info MODIFY COLUMN last_time varchar(32) NULL COMMENT '最后操作时间';
-ALTER TABLE cheliangweizhang.user_info MODIFY COLUMN create_time varchar(32) NULL COMMENT '创建时间';
+ALTER TABLE user_info MODIFY COLUMN last_time varchar(32) NULL COMMENT '最后操作时间';
+ALTER TABLE user_info MODIFY COLUMN create_time varchar(32) NULL COMMENT '创建时间';
